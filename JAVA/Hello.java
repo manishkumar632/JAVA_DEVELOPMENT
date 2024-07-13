4d118a693e59fb0a60bc9074c3cd068c33eff209
Manish Kumar <94158139+manishkumar632@users.noreply.github.com>
2024-07-13 10:29:28 +0000

    updated

--- JAVA/Hello.java
+++ JAVA/Hello.java
@@ -3,5 +3,8 @@ class Hello {
         System.out.println("Hello World");
         int a = 03232; // remember it is treated as octal number;
         System.out.println(a);
+        float f = 23.24f;
+        int x = (int) f;
+        System.out.println(x);
     }
 }
