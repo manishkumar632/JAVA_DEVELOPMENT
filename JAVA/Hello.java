68bb2db64089ba0f9187108f9643f8d4cd200793
Manish Kumar <94158139+manishkumar632@users.noreply.github.com>
2024-07-13 10:24:56 +0000

    updated

--- JAVA/Hello.java
+++ JAVA/Hello.java
@@ -1,7 +1,7 @@
 class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        int a = 03232;
+        int a = 03232; // remember it is treated as octal number;
         System.out.println(a);
     }
 }
